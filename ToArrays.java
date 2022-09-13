1175c109eb4e50c94ab85dcb187eaee78e5ae770
Svartov <100573588+Svartov@users.noreply.github.com>
2022-09-13 20:23:51 +0200

    Add files via upload

--- ToArrays.java
+++ ToArrays.java
@@ -1,5 +1,4 @@
 // Подключаем библиотеки.
-import  java.util.Arrays;
 import  java.io.File;
 import  java.io.PrintWriter;
 import  java.io.IOException;
@@ -7,17 +6,17 @@ import  java.util.Random;
 
 public class ToArrays {
 
-    public static int[][] randomArray(int twoDimArray[][]){
+    public static int[][] randomArray(int twoDimArray[][]){ // Метод который вернёт нам заполненый массив.
         Random rn = new Random(); // Подключаем рандомные цифры.
         for(int i = 0; i < twoDimArray.length; i++){
             for(int j = 0; j < twoDimArray.length; j++){
-                twoDimArray[i][j] = rn.nextInt(123);
+                twoDimArray[i][j] = rn.nextInt(123); // Заполняем массив максимальным числом 123.
             }
         }
-        return twoDimArray;
+        return twoDimArray; // Возвращае готовый массив.
     }
 
-    public static int[][] showArray(int twoDimArray[][], File file){
+    public static void showArray(int[][] twoDimArray, File file){ // Данный метод запишет готовый массив в
         try(PrintWriter pw = new PrintWriter(file)) {
             for (int i = 0; i < twoDimArray.length; i++) {  //идём по строкам
                 for (int j = 0; j < twoDimArray.length; j++) {//идём по столбцам
@@ -28,7 +27,6 @@ public class ToArrays {
         } catch (IOException e) {
             e.printStackTrace();
         }
-        return twoDimArray;
     }
 
 
